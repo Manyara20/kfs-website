70ab62535ad6d5d0c07826ac23a9a91a38ced59a
Najma Said Hussein <najmasaidhussein@gmail.com>
2025-03-18 12:06:27 +0300

    Updated Senior Management

--- src/app/[about]/senior-management/page.jsx
+++ src/app/[about]/senior-management/page.jsx
@@ -1,12 +1,14 @@
 import TopNavBar from "@/components/TopNavBar";
 import MainNavBar from "@/components/MainNavBar";
 import FooterBottom from '@/components/FooterBottom';
+import SeniorManagementSection from "@/pages/about/SeniorManagement";
 
 function KFSBoardPage() {
   return (
     <div>
       <TopNavBar />
       <MainNavBar />
+      <SeniorManagementSection />
       <FooterBottom />
     </div>
   );
